37b3d46e1b8f3b115f527c607361e344b64c160c
xd-sarthak <sarthak.sri02032gmail.com>
2025-12-06 15:55:29 +0530

    Navbar and Sidebar init

---
+++ client/app/dashboardWrapper.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+
+const DashboardWrapper = ({children} : {children: React.ReactNode}) => {
+  return (
+    <div className = "flex min-h-screen bg-gray-50 text-gray-900">
+        <Sidebar />
+        sidebar
+        <main className = "flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64">
+         <Navbar />  
+        {children}
+        </main>
+       
+    </div>
+  )
+}
+
+export default DashboardWrapper;
